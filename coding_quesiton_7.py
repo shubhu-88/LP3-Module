2eaf2f3cca9e8255e277209d8a125c8396fa21a1
shubhu-88 <52776918+shubhu-88@users.noreply.github.com>
2020-06-05 10:45:02 +0530

    Update coding_quesiton_7.py

--- coding_quesiton_7.py
+++ coding_quesiton_7.py
@@ -1,3 +1,12 @@
+'''
+Coding Question 7
+Write a Python program to convert temperatures to and from celsius, Fahrenheit.
+[ Formula : c/5 = f-32/9 [ where c = temperature in celsius and f = temperature in
+Fahrenheit ]
+Expected Output :
+60°C is 140 in Fahrenheit
+45°F is 7 in Celsius
+'''
 def c_f(a,s):
     c1=0
     if(s==1):
@@ -6,11 +15,6 @@ def c_f(a,s):
     else:
         c1=float((9/5)*(c)+32)
         return int(c1)
-    
-
-
-
-
 c=int(input("Enter celsius to convert : "))
 print(c,"°C is",c_f(c,0),"in Fahrenheit")
 f=int(input("Enter fahrenheit to conver: "))
