82856d5ca68cfb2e732f3df9bb39bfc5acab5c00
shubhu-88 <52776918+shubhu-88@users.noreply.github.com>
2020-06-05 10:42:05 +0530

    Update coding_quesiton_6.py

--- coding_quesiton_6.py
+++ coding_quesiton_6.py
@@ -1,3 +1,15 @@
+'''
+Coding Question 6
+Write a Python program to find the first appearance of the substring 'not' and 'poor' from
+a given string, if 'not' follows the 'poor', replace the whole 'not'...' poor' substring with
+'good'. Return the resulting string.
+Sample String :
+'The lyrics is not that poor!'
+'The lyrics is poor!'
+Expected Result :
+'The lyrics is good!'
+'The lyrics is poor!'
+'''
 def sentence_change(str1):
   snot = str1.find('not')
   spoor = str1.find('poor')
@@ -7,4 +19,4 @@ def sentence_change(str1):
   else:
     return str1
 print(sentence_change(input()))
-print(sentence_change(input()))
+
