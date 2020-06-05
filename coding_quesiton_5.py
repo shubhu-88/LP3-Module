e59fb7337c0b7d713d24fb874a46ca33d7b6a178
shubhu-88 <52776918+shubhu-88@users.noreply.github.com>
2020-06-05 10:37:49 +0530

    Update coding_quesiton_5.py

--- coding_quesiton_5.py
+++ coding_quesiton_5.py
@@ -1,3 +1,14 @@
+'''
+Coding Question 5
+You have given a string. You need to remove all the duplicates from the string.
+The final output string should contain each character only once. The respective order of
+the characters inside the string should remain the same. You can only traverse the
+string at once.
+Input string:
+ababacd
+Output string:
+abcd
+'''
 
 dict1 = {}
 a=input()
@@ -11,13 +22,8 @@ for i in range(len(a)):
     key = a[i]
     if not key in dict1.keys():
         dict1.update({key : 1})
+        a2+=key
     else:
         dict1[key] += 1
 
-
-for i in dict1.keys():
-    a2+=str(i)
-
-
-print(a2)    
-    
+print(a2)  
