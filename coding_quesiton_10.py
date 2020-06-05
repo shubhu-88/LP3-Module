9bc63594d8d202336e2b196535f36a6c5dd7650b
shubhu-88 <52776918+shubhu-88@users.noreply.github.com>
2020-06-05 11:01:20 +0530

    Update coding_quesiton_10.py

--- coding_quesiton_10.py
+++ coding_quesiton_10.py
@@ -1,3 +1,13 @@
+'''
+Coding Question 10
+Write a Python program to sort a list of elements using the selection sort algorithm.
+Note: The selection sort improves on the bubble sort by making only one exchange for
+every pass through the list.
+Sample Data:
+[14,46,43,27,57,41,45,21,70]
+Expected Result:
+[14, 21, 27, 41, 43, 45, 46, 57, 70]
+'''
 def selection_sort(alist):
     for i in range(0, len(alist) - 1):
         smallest = i
