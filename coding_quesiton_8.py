0709c2137b049578cb61196b4f18c4f48ad2c6a8
shubhu-88 <52776918+shubhu-88@users.noreply.github.com>
2020-06-05 10:49:36 +0530

    Update coding_quesiton_8.py

--- coding_quesiton_8.py
+++ coding_quesiton_8.py
@@ -1,9 +1,17 @@
-n=int (input())
-#print(n)
-sum1=0
-i=0
-while((n-i)>=0):
-    sum1+=(n-i)
-    i+=2
+'''
+Coding Question 8
+Write a Python program to calculate the sum of the positive integers of n+(n-2)+(n-4)...
+(until n-x =< 0).
+Test Data:
+sum_series(6) -> 12
+sum_series(10) -> 30
+'''
+def sum_series(n):
+    sum1=0
+    i=0
+    while((n-i)>=0):
+        sum1+=(n-i)
+        i+=2
+    return sum1    
 
-print("sum_series(",n,")->",sum1)    
+print(sum_series(int(input())))    
