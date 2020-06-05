ff9b40082a3f907912a3d714461b87ab15c66b42
shubhu-88 <52776918+shubhu-88@users.noreply.github.com>
2020-06-05 10:55:55 +0530

    Update coding_quesiton_9.py

--- coding_quesiton_9.py
+++ coding_quesiton_9.py
@@ -1,3 +1,16 @@
+'''
+Write a Python program to check whether a given a binary tree is a valid binary search
+tree (BST) or not.
+Let a binary search tree (BST) is defined as follows:
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+Example 1:
+2
+/ \
+1 3
+Binary tree [2,1,3], return true.
+'''
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -32,5 +45,3 @@ root.right = TreeNode(3)
  
 result = is_BST(root)
 print(result)
-        
-    
