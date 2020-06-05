59999ee714b3e47aa6993cb5e29f1ef6d2f75738
shubhu-88 <52776918+shubhu-88@users.noreply.github.com>
2020-06-05 10:28:43 +0530

    Update coding_quesiton_4.py

--- coding_quesiton_4.py
+++ coding_quesiton_4.py
@@ -1,3 +1,19 @@
+'''
+Coding Question 4
+Little Robert likes mathematics. Today his teacher has given him two integers and
+asked to find out how many integers can divide both the numbers. Would you like to
+help him in completing his school assignment?
+Input Format :
+There are two integers, a and b as input to the program.
+Output Format:
+Print the number of common factors of a and b. Both the input value should be in a
+range of 1 to 10^12.
+Input:
+10
+15
+Output:
+2
+'''
 a=int(input())
 b=int(input())
 c=0
